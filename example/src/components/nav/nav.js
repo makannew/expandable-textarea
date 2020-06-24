9a1fdee0dd623bbc8ee0b638dbe6eaa33dfcb50e
Makan <makan.edrisi@gmail.com>
2020-06-24 11:33:22 +0800

    active route highlighting added

--- example/src/components/nav/nav.js
+++ example/src/components/nav/nav.js
@@ -1,32 +1,54 @@
 import React from 'react'
 import styles from './nav.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Nav() {
+  const { pathname } = useLocation()
+  const active = styles['active']
   return (
     <div className={styles['nav']}>
       <h3>Expandable-textarea Examples</h3>
-
       <div>
-        <Link className={styles['router-link']} to='/expand-shrink'>
+        <Link
+          className={pathname === '/expand-shrink' ? active : ''}
+          to='/expand-shrink'
+        >
           Expand-shrink
         </Link>
-        <Link className={styles['router-link']} to='/input-like-field'>
+        <Link
+          className={pathname === '/input-like-field' ? active : ''}
+          to='/input-like-field'
+        >
           Input-like-field
         </Link>
-        <Link className={styles['router-link']} to='/focus-by-icon'>
+        <Link
+          className={pathname === '/focus-by-icon' ? active : ''}
+          to='/focus-by-icon'
+        >
           Focus-by-icon
         </Link>
-        <Link className={styles['router-link']} to='/credit-card-format'>
+        <Link
+          className={pathname === '/credit-card-format' ? active : ''}
+          to='/credit-card-format'
+        >
           Credit-card-format
         </Link>
-        <Link className={styles['router-link']} to='/password-format'>
+        <Link
+          className={pathname === '/password-format' ? active : ''}
+          to='/password-format'
+        >
           Password-format
         </Link>
-        <Link className={styles['router-link']} to='/phone-format'>
+        <Link
+          className={pathname === '/phone-format' ? active : ''}
+          to='/phone-format'
+        >
           Phone-format
         </Link>
-        <Link className={styles['router-link']} to='/custom-format'>
+        <Link
+          className={pathname === '/custom-format' ? active : ''}
+          to='/custom-format'
+        >
           Custom-format
         </Link>
       </div>
